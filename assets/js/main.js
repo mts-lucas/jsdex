d89b8d60fe91c285353f3145cddc4164374f8226
Lucas Mateus <lmateus1067@outlook.com>
2022-11-26 20:12:33 -0300

    primeira requisições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('sucesso!');
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+// criando requisições http com js
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
+
