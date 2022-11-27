b251668e7b739c7e665962ad16978799a05e12a1
Lucas Mateus <lmateus1067@outlook.com>
2022-11-27 15:58:54 -0300

    criando lista com js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,36 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToli(pokemon){
+
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">poison</li>
+                    <li class="type">grass</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+
+            </div>
+        </li>
+        `
+}
+
 // criando requisições http com js
+const pokemonList = document.getElementById('pokemonList');
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToli(pokemon);
+        }
+    })
     .catch((error) => console.error(error))
 
