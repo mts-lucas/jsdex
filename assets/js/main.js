4cddd607386c2c4d913cfeed3060ad486072a8b4
Lucas Mateus <lmateus1067@outlook.com>
2022-11-27 23:09:59 -0300

    melhorando o codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToli(pokemon){
 
     return `
@@ -24,14 +20,8 @@ function convertPokemonToli(pokemon){
 // criando requisições http com js
 const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToli(pokemon);
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    pokemonList.innerHTML += pokemons.map(convertPokemonToli).join('');
+})
 
