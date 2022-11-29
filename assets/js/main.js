f03e5be99fd35c9fc3ab7f2918095e9ddad8fe3a
Lucas Mateus <lmateus1067@outlook.com>
2022-11-28 21:03:56 -0300

    estilizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@
 function convertPokemonToli(pokemon){
 
     return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
                 <img src="${pokemon.photo}" alt="${pokemon.name}">
